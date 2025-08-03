3a800ba4c031219aad645b84c05a598d472f6f35
Dr. Brent Hollers <72216475+brent-hollers@users.noreply.github.com>
2025-08-03 15:16:22 -0400

    fixing host url

--- lambda_function.py
+++ lambda_function.py
@@ -4,12 +4,13 @@ from requests_aws4auth import AWS4Auth
 import base64
 import urllib.parse
 import json
+import os
 
 region = 'ap-northeast-1'
 service = 'es'
 credentials = boto3.Session().get_credentials()
 awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
-host = 'https://search-mygoogle-74xgfxo3qbqg4mmm5zzt3a3uye.ap-northeast-1.es.amazonaws.com'
+host = os.environ['HOST']
 index = 'mygoogle'
 url = host + '/' + index + '/_search'
 def get_from_Search(query):
